7bc9b16c5c10f8b0b3177d17888725f3e1f2d3c1
0xEvch <quetwmine@icloud.com>
2025-03-20 16:09:22 +0500

    test: extend test with parameterized check for multiple pages

--- final test/test_product_page.py
+++ final test/test_product_page.py
@@ -1,4 +1,16 @@
 from pages.product_page import ProductPage
+import pytest
+
+promos = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 def test_guest_can_add_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -10,6 +22,14 @@ def test_guest_can_add_product_to_basket(browser):
 
 def test_guest_can_add_other_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_match_book_name_and_price()
+
+@pytest.mark.parametrize('link', promos)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
